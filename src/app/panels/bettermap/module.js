648c91f445f1dd18f5b266f534251b5bf41a26b2
xenoid <hollweck.lucas@gmail.com>
2019-03-06 08:53:15 +0100

    Update module.js

--- src/app/panels/bettermap/module.js
+++ src/app/panels/bettermap/module.js
@@ -109,6 +109,15 @@ function (angular, app, _, L, localRequire) {
     };
 
     $scope.get_data = function(segment,query_id) {
+      	  var
+        _segment,
+        request,
+        boolQuery,
+        boolQuery2, // to help put queries also into filter context
+        queries,
+        sort,
+        request2;
+      
       $scope.require(['./leaflet/plugins'], function () {
         $scope.panel.error =  false;
 
